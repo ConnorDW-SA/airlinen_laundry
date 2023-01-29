90c211e4826a69602c06a3381cd4f03b38eebc67
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-01-29 21:11:16 +0200

    .
    .

--- air-linen/src/components/navbar/Navbar.jsx
+++ air-linen/src/components/navbar/Navbar.jsx
@@ -2,22 +2,51 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { Navbar, Nav, Button } from "react-bootstrap";
 
-export default function Navbar() {
+export default function myNavbar() {
   return (
-    <div className="navbar d-flex justify-content-around mx-5">
-      <img src={logo} alt="logo" className="logo mt-4"></img>
-      <div className="d-flex w-50 justify-content-around my-5">
-        <Link className="links" to="/">
-          Home
-        </Link>
-        <Link to="/about">About</Link>
-        <Link to="/quote">Get a Quote</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/meet-the-team">Meet the Team</Link>
-        <Link to="/privacy-policy">Privacy Policy</Link>
+    <Navbar className="navbar-expand-lg navbar-light p-0 m-0">
+      <Link className="navbar-brand" to="/">
+        <img className="logo ml-5" src={logo} alt="logo"></img>
+      </Link>
+      <div className="collapse navbar-collapse d-flex justify-content-between h-100">
+        <div className="navbar-nav  mr-auto d-flex w-50 justify-content-between h-100 pt-4">
+          <Link className="nav-item nav-link links-links h-100" to="/">
+            Home
+          </Link>
+          <Link className="nav-item nav-link links-links h-100" to="/about">
+            About
+          </Link>
+          <Link className="nav-item nav-link links-links h-100" to="/quote">
+            Get a Quote
+          </Link>
+          <Link className="nav-item nav-link links-links h-100" to="/services">
+            Services
+          </Link>
+          <Link className="nav-item nav-link links-links h-100" to="/contact">
+            Contact
+          </Link>
+          <Link
+            className="nav-item nav-link links-links h-100"
+            to="/meet-the-team"
+          >
+            Meet the Team
+          </Link>
+          <Link
+            className="nav-item nav-link links-links h-100"
+            to="/privacy-policy"
+          >
+            Privacy Policy
+          </Link>
+        </div>
+        <div className="navbar-nav">
+          <p className="navbar-text">Phone: 0800800080</p>
+          <Button className="nav-item nav-link appoint-button">
+            Make an Appointment
+          </Button>
+        </div>
       </div>
-    </div>
+    </Navbar>
   );
 }
--- air-linen/src/components/pages/Home/Home.jsx
+++ air-linen/src/components/pages/Home/Home.jsx
@@ -1,43 +1,61 @@
 import React from "react";
 import Navbar from "../../navbar/Navbar";
-import mainpic from "../../../assets/homepage.jpg";
+import mainpic from "../../../assets/mainpic.jpg";
 import "./Home.css";
-import iconHouse from "../../../assets/iconHouses.svg";
-import iconHotel from "../../../assets/iconHotel.svg";
-import iconAirbnb from "../../../assets/iconAirbnb.svg";
+import { Button } from "react-bootstrap";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className="w-100 h-50">
-        <img src={mainpic} alt="homepage" className="w-100 mainpic"></img>
-        <div className="d-flex justify-content-around text-center mt-5 mb-0 pb-0 w-75 mx-auto">
-          <div className="">
-            <img
-              className="icons-text w-50 mb-2"
-              src={iconHouse}
-              alt="iconHouse"
-            ></img>
-            <h2 className="icons-text">Laundry Guest Houses</h2>
-          </div>
-          <div>
-            <img
-              className="icons-text  w-50 mb-2"
-              src={iconHotel}
-              alt="iconHotel"
-            ></img>
-            <h2 className="icons-text">Laundry Hotels</h2>
-          </div>
-          <div>
-            <img
-              className="icons-text  w-50 mb-2"
-              src={iconAirbnb}
-              alt="iconAirbnb"
-            ></img>
-            <h2 className="icons-text">Laundry Airbnb</h2>
-          </div>
+      <div className="picture-div text-light">
+        <img src={mainpic} alt="mainpic" className="mainpic" />
+        <div className="main-text">
+          <h1 className="display-4 header-text">
+            Quality laundry service <br /> at your fingertips
+          </h1>
+          <h2>We take care of your laundry</h2>
+          <Button className="w-25">Explore our Services</Button>
         </div>
+      </div>
+      <div className="container mt-5">
+        <h1 className="text-center">How it works</h1>
+      </div>
+      <div className="d-flex text-center justify-content-around mx-5 mt-5 pt-5">
+        <div className="w-25">
+          <img src="https://via.placeholder.com/50 " alt="how it works" />
+          <h2>We collect your laundry</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            vel excepturi, fuga rem aliquam blanditiis dolore quo, omnis nihil
+            laborum unde odio vero eos assumenda natus quaerat repudiandae
+            voluptatibus voluptatem.
+          </p>
+        </div>
+        <div className="w-25">
+          <img src="https://via.placeholder.com/50 " alt="how it works" />
+          <h2>We wash your laundry</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            vel excepturi, fuga rem aliquam blanditiis dolore quo, omnis nihil
+            laborum unde odio vero eos assumenda natus quaerat repudiandae
+            voluptatibus voluptatem.
+          </p>
+        </div>
+        <div className="w-25">
+          <img src="https://via.placeholder.com/50 " alt="how it works" />
+          <h2>We deliver your laundry</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            vel excepturi, fuga rem aliquam blanditiis dolore quo, omnis nihil
+            laborum unde odio vero eos assumenda natus quaerat repudiandae
+            voluptatibus voluptatem.
+          </p>
+        </div>
+      </div>
+      <hr></hr>
+      <div>
+        <h1>Services we offer</h1>
       </div>
     </div>
   );
