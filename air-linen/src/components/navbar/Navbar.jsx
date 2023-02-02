6ccd141f0339beb124b69ea42b10f9b51d4ff918
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-02-02 10:51:45 +0200

    .

--- air-linen/src/components/navbar/Navbar.jsx
+++ air-linen/src/components/navbar/Navbar.jsx
@@ -7,22 +7,21 @@ import phone from "../../assets/phone.png";
 
 export default function myNavbar() {
   return (
-    <Navbar bg="white" expand="lg" className="w-100 m-0 p-0 navvy">
+    <Navbar bg="white" expand="lg" className="w-100 m-0 p-0">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto ml-5 d-flex justify-content-between w-50">
-          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/">
+            <h1 className="logo">AirLinen.</h1>
+          </Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/quote">Get a Quote</Nav.Link>
           <Nav.Link href="/services">Services</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
           <Nav.Link href="/meet-the-team">Meet the Team</Nav.Link>
           <Nav.Link href="/privacy-policy">Privacy Policy</Nav.Link>
-        </Nav>
-        <Nav className="phone-nav bg-primary">
-          <Nav.Link className="text-white" href="tel:+27791116858">
-            <img src={phone} className="phone mr-2" alt="phone"></img> | +(27)
-            079 111 6858
+          <Nav.Link href="tel:+27791116858">
+            <img src={phone} alt="phone"></img> | +(27) 079 111 6858
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
