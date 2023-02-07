9ec043c6e87b7079675c35cb788b2e31e2473937
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-02-07 18:01:56 +0200

    .

--- air-linen/src/components/navbar/Navbar.jsx
+++ air-linen/src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import { Navbar, NavDropdown } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 import phone from "../../assets/phone.png";
-
-import CustomDropdown from "./Dropdown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function MyNavbar() {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   return (
     <Navbar bg="white" className="w-100 fixed-top">
       <div className="d-flex justify-content-between w-100 mx-5 ">
@@ -27,7 +29,28 @@ export default function MyNavbar() {
             111 6858
           </a>
         </div>
-        <CustomDropdown />
+        <button
+          className="button d-lg-none"
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        {showDropdown && (
+          <div
+            className="dropdown-container d-flex flex-column"
+            style={{ top: "100%", left: 0 }}
+          >
+            <a className="p-3" href="/contact">
+              Contact
+            </a>
+            <a className="p-3" href="/privacy-policy">
+              Privacy Policy
+            </a>
+            <a className="p-3" href="tel:+27791116858">
+              +(27) 079 111 6858
+            </a>
+          </div>
+        )}
       </div>
     </Navbar>
   );
--- air-linen/src/components/pages/Home/Home.jsx
+++ air-linen/src/components/pages/Home/Home.jsx
@@ -12,10 +12,12 @@ export default function Home() {
       <Navbar />
       <div className="hero-section mx-5">
         <img src={secondpic} alt="main" className="main-pic" />
-        <h1 className="display-5 ml-5">We take care of your laundry</h1>
+        <h1 className="display-5 ml-5 word-wrap w-50">
+          We take care of your laundry
+        </h1>
       </div>
       <div className="enquire container-fluid ml-5 pl-4 mt-5">
-        <p className="enquire-text">
+        <p className="enquire-text word-wrap w-75 ">
           AirLinen is a growing laundry service that aims to provide only the
           best service for our clients. <br /> Focused mainly on Short term
           lets, airbnb's, guest houses, hotels, spas and salons, we have quickly
@@ -27,21 +29,21 @@ export default function Home() {
       </div>
       <div className="services container-fluid my-5">
         <h1 className="text-center mb-5">Services we offer</h1>
-        <div className="d-flex w-75 m-auto backgroundpic">
+        <div className="d-lg-flex d-md-block w-75 m-auto backgroundpic">
           <img src={mainpic} alt="second" className="pic-dim" />
           <div>
-            <h1 className="text-center pt-5 ml-5">Daily Collection Services</h1>
-            <p className="text-center mt-2 pt-5 mx-5">
+            <h1 className="text-left pt-5 ml-5">Daily Collection Services</h1>
+            <p className="text-left mt-2 pt-5 mx-5 pb-4">
               We provide daily collection and dropoff services for all your
               laundry needs. We operate in and around the City of Cape Town.
               Simply let us know which location and time suits you!
             </p>
           </div>
         </div>
-        <div className="d-flex w-75 m-auto backgroundpic">
+        <div className="d-lg-flex d-md-block w-75 m-auto backgroundpic">
           <div>
-            <h1 className="text-center pt-5 ml-5">Our Laundry guarantee</h1>
-            <p className="text-center mt-2 pt-5 mx-5">
+            <h1 className="text-left pt-5 ml-5">Our Laundry guarantee</h1>
+            <p className="text-left mt-2 pt-5 mx-5">
               We strive to achieve the best quality results for commercial
               businesses, or for individual customers.
             </p>
